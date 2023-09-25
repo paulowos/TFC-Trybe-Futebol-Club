a84e51bfdc60534dc755c610864924f56437024b
paulowos <paulowos@gmail.com>
2023-09-25 18:28:09 -0300

    test: req.02 - Teams

---
+++ app/backend/src/tests/Team.test.ts
@@ -0,0 +1,31 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import TeamSequelizeModel from '../database/models/TeamSequelizeModel';
+import { teams } from './mocks/team.mock';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teams', () => {
+  let response: Response;
+
+  beforeEach(function () {
+    sinon.restore();
+  });
+
+  it('GET /teams should return a list of teams', async function () {
+    sinon.stub(TeamSequelizeModel, 'findAll').resolves(teams as any);
+
+    const response = await chai.request(app).get('/teams');
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal(teams);
+  });
+});
