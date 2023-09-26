3288b5c75d140ed0f65d2338019503fe8c384523
paulowos <paulowos@gmail.com>
2023-09-26 11:14:07 -0300

    feat: req.12 - User Router

--- app/backend/src/routers/userRouter.ts
+++ app/backend/src/routers/userRouter.ts
@@ -1,12 +1,20 @@
 import { Request, Response, Router } from 'express';
 import UserController from '../controllers/UserController';
 import UserMiddleware from '../middlewares/UserMiddleware';
+import AuthMiddleware from '../middlewares/AuthMiddleware';
 
 const userController = new UserController();
+
+const tokenMiddlewares = [
+  AuthMiddleware.verifyHeader,
+  AuthMiddleware.validateToken,
+];
 
 const router = Router();
 
 router.post('/', UserMiddleware.login, (req: Request, res: Response) =>
   userController.login(req, res));
+
+router.get('/role', tokenMiddlewares, UserController.getRole);
 
 export default router;
