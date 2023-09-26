73711fbe2a1b7764017f3cb742a3bf18df9d5f7e
paulowos <paulowos@gmail.com>
2023-09-25 21:25:43 -0300

    feat: req.06 - User Migration

---
+++ app/backend/src/database/migrations/02-create-users.ts
@@ -0,0 +1,18 @@
+import { DataTypes, Model, QueryInterface } from 'sequelize';
+import IUser from '../../Interfaces/User';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IUser>>('users', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      username: { type: DataTypes.STRING, allowNull: false },
+      role: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, allowNull: false },
+      password: { type: DataTypes.STRING, allowNull: false },
+    });
+  },
+
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('users');
+  },
+};
