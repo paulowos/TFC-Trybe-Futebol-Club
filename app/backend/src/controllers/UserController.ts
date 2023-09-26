0bc74c1f1d688ca57086d7d2ffd2efd22fa4a50f
paulowos <paulowos@gmail.com>
2023-09-25 22:33:21 -0300

    feat: req.08 - User Controller

---
+++ app/backend/src/controllers/UserController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import UserService from '../services/UserService';
+import statusHttp from '../utils/statusHttp';
+
+export default class UserController {
+  constructor(private userService = new UserService()) {}
+
+  async login(req: Request, res: Response) {
+    const { email, password } = req.body;
+    const { status, body } = await this.userService.login(email, password);
+    res.status(statusHttp[status]).json(body);
+  }
+}
