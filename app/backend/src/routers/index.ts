ef3c48d838fbabbe9e6d8c351e70d8e07168127d
paulowos <paulowos@gmail.com>
2023-09-25 22:35:51 -0300

    feat: req.08 - User Router

--- app/backend/src/routers/index.ts
+++ app/backend/src/routers/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import teamRouter from './teamRouter';
+import userRouter from './userRouter';
 
 const mainRouter = Router();
 
 mainRouter.use('/teams', teamRouter);
+mainRouter.use('/login', userRouter);
 
 export default mainRouter;
