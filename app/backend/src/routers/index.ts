638876ea16c7bdce963c5a486ae1b565c0ae424f
paulowos <paulowos@gmail.com>
2023-09-27 11:42:00 -0300

    feat: req.15 - router

--- app/backend/src/routers/index.ts
+++ app/backend/src/routers/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+
 import teamRouter from './teamRouter';
 import userRouter from './userRouter';
+import matchRouter from './matchRouter';
 
 const mainRouter = Router();
 
 mainRouter.use('/teams', teamRouter);
 mainRouter.use('/login', userRouter);
+mainRouter.use('/matches', matchRouter);
 
 export default mainRouter;
