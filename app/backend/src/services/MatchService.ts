2042cdf9dd94f55342e5d4542206c10d23b75990
paulowos <paulowos@gmail.com>
2023-09-27 11:33:11 -0300

    feat: req.15 - service

---
+++ app/backend/src/services/MatchService.ts
@@ -0,0 +1,13 @@
+import ServiceResponse from '../Interfaces/ServiceResponse';
+import IMatchModel from '../Interfaces/MatchModel';
+import MatchModel from '../models/MatchModel';
+import IMatch from '../Interfaces/Match';
+
+export default class MatchService {
+  constructor(private matchModel: IMatchModel = new MatchModel()) {}
+
+  async getAll(): Promise<ServiceResponse<IMatch[]>> {
+    const body = await this.matchModel.getAll();
+    return { status: 'OK', body };
+  }
+}
