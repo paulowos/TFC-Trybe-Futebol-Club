fc55590298731ea61f4e653036981b82aa541bb9
paulowos <paulowos@gmail.com>
2023-09-27 19:09:02 -0300

    feat: req.17 - router

--- app/backend/src/routers/matchRouter.ts
+++ app/backend/src/routers/matchRouter.ts
@@ -1,11 +1,22 @@
 import { Request, Response, Router } from 'express';
 import MatchController from '../controllers/MatchController';
+import AuthMiddleware from '../middlewares/AuthMiddleware';
 
 const matchController = new MatchController();
+
+const tokenMiddlewares = [
+  AuthMiddleware.verifyHeader,
+  AuthMiddleware.validateToken,
+];
 
 const router = Router();
 
 router.get('/', (req: Request, res: Response) =>
   matchController.getAll(req, res));
+
+// router.use(tokenMiddlewares);
+
+router.patch('/:id/finish', tokenMiddlewares, (req: Request, res: Response) =>
+  matchController.finishMatch(req, res));
 
 export default router;
