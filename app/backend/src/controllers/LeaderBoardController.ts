ca6a5bc5a34fda60c42ae2135aa79b896116acc8
paulowos <paulowos@gmail.com>
2023-09-28 15:26:32 -0300

    feat: leaderboard/home - controller

---
+++ app/backend/src/controllers/LeaderBoardController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import LeaderBoardService from '../services/LeaderBoardService';
+import statusHttp from '../utils/statusHttp';
+
+export default class LeaderBoardController {
+  constructor(private leaderBoardService = new LeaderBoardService()) {}
+
+  async getAllHome(_req: Request, res: Response) {
+    const { status, body } = await this.leaderBoardService.getAllHome();
+    res.status(statusHttp[status]).json(body);
+  }
+}
